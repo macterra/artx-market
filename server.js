e2325a3d6dc40ffbfb0fe4c2dff5ca87e9f8c160
David McFadzean <davidmc@gmail.com>
2023-06-01 19:50:00 -0400

    Added lnurl auth

--- server.js
+++ server.js
@@ -4,8 +4,27 @@ const multer = require('multer');
 const fs = require('fs');
 const crypto = require('crypto');
 const sharp = require('sharp');
+const passport = require('passport');
+const LnurlAuth = require('passport-lnurl-auth');
+const session = require('express-session');
 
 const app = express();
+
+const config = {
+	host: 'localhost',
+	port: 5000,
+	url: null,
+};
+
+if (!config.url) {
+	config.url = 'http://' + config.host + ':' + config.port;
+}
+
+app.use(session({
+	secret: '12345',
+	resave: true,
+	saveUninitialized: true,
+}));
 
 // Serve the React frontend
 app.use(express.static(path.join(__dirname, 'frontend/build')));
@@ -32,6 +51,46 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const map = {
+	user: new Map(),
+};
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	done(null, map.user.get(id) || null);
+});
+
+passport.use(new LnurlAuth.Strategy(function(linkingPublicKey, done) {
+	let user = map.user.get(linkingPublicKey);
+	if (!user) {
+		user = { id: linkingPublicKey };
+		map.user.set(linkingPublicKey, user);
+	}
+	done(null, user);
+}));
+
+app.use(passport.authenticate('lnurl-auth'));
+
+app.get('/login',
+	function(req, res, next) {
+		if (req.user) {
+			// Already authenticated.
+			return res.redirect('/');
+		}
+		next();
+	},
+	new LnurlAuth.Middleware({
+		callbackUrl: config.url + '/login',
+		cancelUrl: config.url
+	})
+);
 
 const upload = multer({ storage });
 
