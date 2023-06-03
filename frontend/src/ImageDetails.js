01f3621a69c83eddbd87ece0f97811a634644b3b
David McFadzean <davidmc@gmail.com>
2023-06-03 15:15:16 -0400

    Display metadata in table

--- frontend/src/ImageDetails.js
+++ frontend/src/ImageDetails.js
@@ -1,42 +1,66 @@
-
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+  } from '@mui/material';
+
 const ImageDetails = () => {
-  const { hash } = useParams();
-  const [image, setImage] = useState(null);
+    const { hash } = useParams();
+    const [image, setImage] = useState(null);
 
-  useEffect(() => {
-    const fetchMetadata = async () => {
-      try {
-        const response = await fetch(`/data/assets/${hash}/meta.json`);
-        const metadata = await response.json();
-        setImage(metadata);
-      } catch (error) {
-        console.error('Error fetching image metadata:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchMetadata = async () => {
+            try {
+                const response = await fetch(`/data/assets/${hash}/meta.json`);
+                const metadata = await response.json();
+                setImage(metadata);
+            } catch (error) {
+                console.error('Error fetching image metadata:', error);
+            }
+        };
 
-    fetchMetadata();
-  }, [hash]);
+        fetchMetadata();
+    }, [hash]);
 
-  if (!image) {
-    return <p>Loading...</p>;
-  }
+    if (!image) {
+        return <p>Loading...</p>;
+    }
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <div style={{ width: '50%', padding: '16px' }}>
-        <img src={image.asset.path} alt={image.asset.filename} style={{ width: '100%', height: 'auto' }} />
-      </div>
-      <div style={{ width: '50%', padding: '16px' }}>
-        <h2>Metadata:</h2>
-        <p>Filename: {image.asset.fileName}</p>
-        <p>File size: {image.asset.fileSize} bytes</p>
-        {/* Add any other metadata you want to display */}
-      </div>
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style={{ width: '50%', padding: '16px' }}>
+                <img src={image.asset.path} alt={image.asset.filename} style={{ width: '100%', height: 'auto' }} />
+            </div>
+
+            <div style={{ width: '50%', padding: '16px' }}>
+                <h2>Metadata:</h2>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Filename:</TableCell>
+                                <TableCell>{image.asset.filename}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>File size:</TableCell>
+                                <TableCell>{image.asset.fileSize} bytes</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Image size:</TableCell>
+                                <TableCell>{image.image.width} x {image.image.height}</TableCell>
+                            </TableRow>
+                            {/* Add any other metadata you want to display */}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    );
 };
 
 export default ImageDetails;
