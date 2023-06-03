f9d3ce9d6b02207fbd2ab6511abeeb5f597ef9f3
David McFadzean <davidmc@gmail.com>
2023-06-03 15:31:40 -0400

    Added metadata

--- frontend/src/ImageDetails.js
+++ frontend/src/ImageDetails.js
@@ -11,14 +11,14 @@ import {
 
 const ImageDetails = () => {
     const { hash } = useParams();
-    const [image, setImage] = useState(null);
+    const [metadata, setMetadata] = useState(null);
 
     useEffect(() => {
         const fetchMetadata = async () => {
             try {
                 const response = await fetch(`/data/assets/${hash}/meta.json`);
                 const metadata = await response.json();
-                setImage(metadata);
+                setMetadata(metadata);
             } catch (error) {
                 console.error('Error fetching image metadata:', error);
             }
@@ -27,32 +27,47 @@ const ImageDetails = () => {
         fetchMetadata();
     }, [hash]);
 
-    if (!image) {
+    if (!metadata) {
         return <p>Loading...</p>;
     }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div style={{ width: '50%', padding: '16px' }}>
-                <img src={image.asset.path} alt={image.asset.filename} style={{ width: '100%', height: 'auto' }} />
+                <img src={metadata.asset.path} alt={metadata.asset.originalName} style={{ width: '100%', height: 'auto' }} />
             </div>
-
             <div style={{ width: '50%', padding: '16px' }}>
                 <h2>Metadata:</h2>
                 <TableContainer>
                     <Table>
                         <TableBody>
                             <TableRow>
-                                <TableCell>Filename:</TableCell>
-                                <TableCell>{image.asset.filename}</TableCell>
+                                <TableCell>Title:</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description:</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Tags:</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Creator:</TableCell>
+                                <TableCell>{metadata.asset.creator}</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell>File size:</TableCell>
-                                <TableCell>{image.asset.fileSize} bytes</TableCell>
+                                <TableCell>{metadata.asset.fileSize} bytes</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell>Image size:</TableCell>
-                                <TableCell>{image.image.width} x {image.image.height}</TableCell>
+                                <TableCell>{metadata.image.width} x {metadata.image.height}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Image format:</TableCell>
+                                <TableCell>{metadata.image.format}</TableCell>
                             </TableRow>
                             {/* Add any other metadata you want to display */}
                         </TableBody>
