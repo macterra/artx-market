83665712e18f1382a4ad6f4150767e08fdc61142
David McFadzean <davidmc@gmail.com>
2023-06-18 16:37:28 -0400

    Simplified CollectionView

--- frontend/src/CollectionView.js
+++ frontend/src/CollectionView.js
@@ -1,44 +1,31 @@
 
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Tabs, Tab } from '@mui/material';
 import ImageGrid from './ImageGrid';
 
 const CollectionView = ({ navigate }) => {
     const { userId, collId } = useParams();
-
     const [profile, setProfile] = useState(null);
-    const [tab, setTab] = useState(null);
     const [collection, setCollection] = useState(null);
+    const [collectionName, setCollectionName] = useState(null);
+    const [collectionId, setCollectionId] = useState(null);
     const [refreshKey, setRefreshKey] = useState(0);
 
     useEffect(() => {
         const fetchProfile = async () => {
             try {
-                if (!userId) {
-                    const response = await fetch(`/api/profile`);
-                    const profileData = await response.json();
+                let response = await fetch(`/api/profile/${userId}`);
+                const profileData = await response.json();
+                setProfile(profileData);
 
-                    if (!profileData.id) {
-                        navigate('/');
-                    } else {
-                        navigate(`/profile/${profileData.id}/${profileData.defaultCollection}`);
-                    }
-                } else if (!collId) {
-                    const response = await fetch(`/api/profile/${userId}`);
-                    const profileData = await response.json();
-                    navigate(`/profile/${userId}/${profileData.defaultCollection}`);
-                } else {
-                    const response = await fetch(`/api/profile/${userId}`);
-                    const profileData = await response.json();
-                    setProfile(profileData);
+                response = await fetch(`/api/collection/${userId}/${collId}`);
+                const collectionData = await response.json();
+                const collectionId = parseInt(collId, 10);
+                const collectionName = profileData.collections[collectionId].name;
 
-                    const response2 = await fetch(`/api/collection/${userId}/${collId}`);
-                    const collectionData = await response2.json();
-
-                    setCollection(collectionData);
-                    setTab(parseInt(collId, 10));
-                }
+                setCollection(collectionData);
+                setCollectionId(collectionId);
+                setCollectionName(collectionName);
             } catch (error) {
                 console.error('Error fetching profile data:', error);
             }
@@ -72,28 +59,11 @@ const CollectionView = ({ navigate }) => {
         }
     };
 
-    const handleCollectionChange = (event, newIndex) => {
-        setTab(newIndex);
-        navigate(`/profile/${userId}/${newIndex}`);
-    };
-
     return (
         <>
-            <p style={{ textAlign: 'left' }}>Collections</p>
-            <Tabs
-                value={tab}
-                onChange={handleCollectionChange}
-                indicatorColor="primary"
-                textColor="primary"
-                variant="scrollable"
-                scrollButtons="auto"
-            >
-                {profile.collections.map((collection, index) => (
-                    <Tab key={index} label={collection.name} />
-                ))}
-            </Tabs>
+            <p>{collectionName}</p>
             <ImageGrid collection={collection} />
-            {tab === 0 && <input type="file" onChange={handleUpload} />}
+            {collectionId === 0 && <input type="file" onChange={handleUpload} />}
         </>
     );
 };
