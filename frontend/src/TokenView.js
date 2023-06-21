bf22a7e1f5795afd8ccc5f83e7f44d2dbec9d0ee
David McFadzean <davidmc@gmail.com>
2023-06-21 17:15:12 -0400

    Owner cell alignment

--- frontend/src/TokenView.js
+++ frontend/src/TokenView.js
@@ -112,19 +112,21 @@ const TokenView = ({ metadata }) => {
                         <TableRow>
                             <TableCell>Owner:</TableCell>
                             <TableCell>
-                                {nfts[0].owner.pfp &&
-                                    <img
-                                        src={nfts[0].owner.pfp}
-                                        alt=""
-                                        style={{
-                                            width: '30px',
-                                            height: '30px',
-                                            objectFit: 'cover',
-                                            marginRight: '16px',
-                                            borderRadius: '50%',
-                                        }}
-                                    />}
-                                {nfts[0].owner.name}
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    {nfts[0].owner.pfp &&
+                                        <img
+                                            src={nfts[0].owner.pfp}
+                                            alt=""
+                                            style={{
+                                                width: '30px',
+                                                height: '30px',
+                                                objectFit: 'cover',
+                                                marginRight: '16px',
+                                                borderRadius: '50%',
+                                            }}
+                                        />}
+                                    {nfts[0].owner.name}
+                                </div>
                             </TableCell>
                         </TableRow>
                     }
@@ -145,18 +147,20 @@ const TokenView = ({ metadata }) => {
                                                 <TableRow key={index}>
                                                     <TableCell>{nft.asset.title}</TableCell>
                                                     <TableCell>
-                                                        {nft.owner.pfp &&
-                                                            <img
-                                                                src={nft.owner.pfp}
-                                                                alt=""
-                                                                style={{
-                                                                    width: '30px',
-                                                                    height: '30px',
-                                                                    objectFit: 'cover',
-                                                                    marginRight: '16px',
-                                                                    borderRadius: '50%',
-                                                                }}
-                                                            />} {nft.owner.name}
+                                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                                            {nft.owner.pfp &&
+                                                                <img
+                                                                    src={nft.owner.pfp}
+                                                                    alt=""
+                                                                    style={{
+                                                                        width: '30px',
+                                                                        height: '30px',
+                                                                        objectFit: 'cover',
+                                                                        marginRight: '16px',
+                                                                        borderRadius: '50%',
+                                                                    }}
+                                                                />} {nft.owner.name}
+                                                        </div>
                                                     </TableCell>
                                                 </TableRow>
                                             ))}
