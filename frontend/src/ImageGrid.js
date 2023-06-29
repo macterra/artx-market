1e5ec595ecaf70db306a5a963cc34cc2dbb71c1d
David McFadzean <davidmc@gmail.com>
2023-06-29 15:35:17 -0400

    Center images

--- frontend/src/ImageGrid.js
+++ frontend/src/ImageGrid.js
@@ -26,7 +26,7 @@ const ImageGrid = ({ collection }) => {
     };
 
     return (
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'left' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
             {images.map((metadata, index) => (
                 <Link key={index} to={`/asset/${metadata.asset.xid}`} style={imageCardStyle}>
                     <ImageCard key={index} metadata={metadata} />
