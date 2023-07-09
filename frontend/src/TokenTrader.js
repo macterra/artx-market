c3bfdcde9262d5d0a5cbeafcdd67a2834b4730c6
David McFadzean <davidmc@gmail.com>
2023-07-09 13:27:55 -0400

    Added lightning payments for NFT trades

--- frontend/src/TokenTrader.js
+++ frontend/src/TokenTrader.js
@@ -9,6 +9,7 @@ import {
     Paper,
     TextField,
     Button,
+    Modal,
 } from '@mui/material';
 
 const fetchExchangeRate = async () => {
@@ -21,6 +22,10 @@ const TokenTrader = ({ metadata, setRefreshKey }) => {
     const [ownedNfts, setOwnedNfts] = useState(0);
     const [listedNfts, setListedNfts] = useState(0);
     const [exchangeRate, setExchangeRate] = useState(null);
+    const [charge, setCharge] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [invoiceUrl, setInvoiceUrl] = useState('');
+    const [nftSale, setNftSale] = useState(null);
 
     useEffect(() => {
         const fetchProfile = async () => {
@@ -129,6 +134,57 @@ const TokenTrader = ({ metadata, setRefreshKey }) => {
     };
 
     const handleBuyClick = async (nft) => {
+        setNftSale(nft);
+
+        try {
+            const response = await fetch('/api/charge', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', },
+                body: JSON.stringify({ description: 'buy NFT', amount: nft.nft.price }),
+            });
+
+            const chargeData = await response.json();
+            console.log(chargeData);
+
+            if (chargeData.url) {
+                setCharge(chargeData);
+                setInvoiceUrl(chargeData.url);
+                setModalOpen(true);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleInvoiceClose = async () => {
+        setModalOpen(false);
+
+        try {
+            const response = await fetch(`/api/charge/${charge.id}`);
+            const chargeData = await response.json();
+            console.log(chargeData);
+
+            if (chargeData.paid) {
+                const response = await fetch(`/api/asset/${nftSale.asset.xid}/buy`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json', },
+                    body: JSON.stringify({ price: nftSale.nft.price, chargeId: charge.id }),
+                });
+
+                if (response.ok) {
+                    setRefreshKey((prevKey) => prevKey + 1);
+                } else {
+                    const data = await response.json();
+                    console.error('Error:', data.message);
+                    alert(data.message);
+                }
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleBuyClickX = async (nft) => {
         try {
             const response = await fetch(`/api/asset/${nft.asset.xid}/buy`, {
                 method: 'POST',
@@ -188,73 +244,95 @@ const TokenTrader = ({ metadata, setRefreshKey }) => {
     };
 
     return (
-        <TableContainer>
-            <Table>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Title:</TableCell>
-                        <TableCell>{metadata.asset.title}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Editions:</TableCell>
-                        <TableCell>{metadata.token.editions > 1 ? metadata.token.editions : "1 of 1"}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Exchange rate:</TableCell>
-                        <TableCell>{exchangeRate} USD/BTC</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Sell:</TableCell>
-                        <TableCell>
-                            {ownedNfts.length > 0 &&
-                                <TableContainer component={Paper} style={{ maxHeight: '300px', overflow: 'auto' }}>
-                                    <Table>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>Edition</TableCell>
-                                                <TableCell>Price (USD)</TableCell>
-                                                <TableCell>Price (sats)</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {ownedNfts.map((nft, index) => (
-                                                <SellerTableRow key={index} nft={nft} />
-                                            ))}
-                                        </TableBody>
+        <>
+            <TableContainer>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Title:</TableCell>
+                            <TableCell>{metadata.asset.title}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Editions:</TableCell>
+                            <TableCell>{metadata.token.editions > 1 ? metadata.token.editions : "1 of 1"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Exchange rate:</TableCell>
+                            <TableCell>{exchangeRate} USD/BTC</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Sell:</TableCell>
+                            <TableCell>
+                                {ownedNfts.length > 0 &&
+                                    <TableContainer component={Paper} style={{ maxHeight: '300px', overflow: 'auto' }}>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>Edition</TableCell>
+                                                    <TableCell>Price (USD)</TableCell>
+                                                    <TableCell>Price (sats)</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {ownedNfts.map((nft, index) => (
+                                                    <SellerTableRow key={index} nft={nft} />
+                                                ))}
+                                            </TableBody>
 
-                                    </Table>
-                                </TableContainer>
-                            }
-                            {ownedNfts.length < 1 && "None currently owned"}
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Buy:</TableCell>
-                        <TableCell>
-                            {listedNfts.length > 0 &&
-                                <TableContainer component={Paper} style={{ maxHeight: '300px', overflow: 'auto' }}>
-                                    <Table>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>Edition</TableCell>
-                                                <TableCell>Price (USD)</TableCell>
-                                                <TableCell>Price (sats)</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {listedNfts.map((nft, index) => (
-                                                <BuyerTableRow nft={nft} />
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            }
-                            {listedNfts.length < 1 && "None currently listed"}
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer>
+                                        </Table>
+                                    </TableContainer>
+                                }
+                                {ownedNfts.length < 1 && "None currently owned"}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Buy:</TableCell>
+                            <TableCell>
+                                {listedNfts.length > 0 &&
+                                    <TableContainer component={Paper} style={{ maxHeight: '300px', overflow: 'auto' }}>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>Edition</TableCell>
+                                                    <TableCell>Price (USD)</TableCell>
+                                                    <TableCell>Price (sats)</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {listedNfts.map((nft, index) => (
+                                                    <BuyerTableRow nft={nft} />
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                }
+                                {listedNfts.length < 1 && "None currently listed"}
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Modal
+                open={modalOpen}
+                onClose={() => handleInvoiceClose()}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <div style={{ backgroundColor: '#282c34', padding: '1em', width: '50vw', height: '100vh', overflow: 'auto' }}>
+                    <iframe
+                        src={invoiceUrl}
+                        title="Invoice"
+                        width="100%"
+                        height="90%"
+                        style={{ border: 'none' }}
+                    />
+                    <Button onClick={() => handleInvoiceClose()}>Close</Button>
+                </div>
+            </Modal>
+        </>
     );
 };
 
