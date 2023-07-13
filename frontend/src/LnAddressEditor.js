56e9ad9f2d85d8c643f3c43dee7ef2b2a7535c8d
David McFadzean <davidmc@gmail.com>
2023-07-13 17:25:09 -0400

    Added LnAddressEditor

---
+++ frontend/src/LnAddressEditor.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Box, Button, Grid, TextField } from '@mui/material';
+
+const LnAddressEditor = ({ profile }) => {
+    const [address, setAddress] = useState(profile.deposit);
+    const [invoice, setInvoice] = useState(null);
+
+    const handleSaveAddress = async () => {
+        try {
+            const response = await fetch('/api/profile', {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json', },
+                body: JSON.stringify({ deposit: address }),
+            });
+
+            if (response.ok) {
+                profile.deposit = address;
+                setInvoice(null);
+            } else {
+                const data = await response.json();
+                console.error('Error updating profile:', data.message);
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error('Error updating profile:', error);
+        }
+    };
+
+    const handleTestAddress = async () => {
+        try {
+            const response = await fetch(`/api/profile/${profile.xid}/invoice`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', },
+                body: JSON.stringify({ amount: 10 }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setInvoice(`lightning:${data.invoice}`);
+            } else {
+                const data = await response.json();
+                console.error('Error:', data.message);
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <form>
+            <Grid container direction="column" alignItems="center">
+                <Grid item>
+                    <TextField
+                        label="Lightning Address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                        sx={{ width: '20ch' }}
+                    />
+                </Grid>
+                <Grid item>
+                    <Box sx={{ marginTop: 2 }}>
+                        <Grid container spacing={2}>
+                            <Grid item>
+                                <Button variant="contained" color="primary" onClick={handleSaveAddress}>
+                                    Save
+                                </Button>
+                            </Grid>
+                            {address &&
+                                <Grid item>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleTestAddress}
+                                        disabled={!!invoice}>
+                                        Test
+                                    </Button>
+                                </Grid>
+                            }
+                        </Grid>
+                    </Box>
+                </Grid>
+                {invoice &&
+                    <Grid item>
+                        <div>
+                            <a href={invoice} onClick={() => setInvoice(null)}>⚡ zap 10 sats ⚡</a>
+                        </div>
+                    </Grid>
+                }
+            </Grid>
+        </form>
+    );
+};
+
+export default LnAddressEditor;
--- frontend/src/ProfileEditor.js
+++ frontend/src/ProfileEditor.js
@@ -1,14 +1,13 @@
 
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Grid, TextField, Tab, Tabs } from '@mui/material';
+import { Box, Button, TextField, Tab, Tabs } from '@mui/material';
 import CollectionEditor from './CollectionEditor';
+import LnAddressEditor from './LnAddressEditor';
 
 const ProfileEditor = ({ navigate }) => {
     const [profile, setProfile] = useState({});
     const [name, setName] = useState('');
     const [tagline, setTagline] = useState('');
-    const [address, setAddress] = useState('');
-    const [invoice, setInvoice] = useState(null);
     const [tab, setTab] = useState(null);
 
     useEffect(() => {
@@ -19,7 +18,6 @@ const ProfileEditor = ({ navigate }) => {
                 setProfile(data);
                 setName(data.name);
                 setTagline(data.tagline);
-                setAddress(data.deposit);
                 setTab("name");
             } catch (error) {
                 console.error('Error fetching profile data:', error);
@@ -48,48 +46,6 @@ const ProfileEditor = ({ navigate }) => {
             }
         } catch (error) {
             console.error('Error updating profile:', error);
-        }
-    };
-
-    const handleSaveAddress = async () => {
-        try {
-            const response = await fetch('/api/profile', {
-                method: 'PATCH',
-                headers: { 'Content-Type': 'application/json', },
-                body: JSON.stringify({ deposit: address }),
-            });
-
-            if (response.ok) {
-                profile.deposit = address;
-                setInvoice(null);
-            } else {
-                const data = await response.json();
-                console.error('Error updating profile:', data.message);
-                alert(data.message);
-            }
-        } catch (error) {
-            console.error('Error updating profile:', error);
-        }
-    };
-
-    const handleTestAddress = async () => {
-        try {
-            const response = await fetch(`/api/profile/${profile.xid}/invoice`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', },
-                body: JSON.stringify({ amount: 10 }),
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                setInvoice(`lightning:${data.invoice}`);
-            } else {
-                const data = await response.json();
-                console.error('Error:', data.message);
-                alert(data.message);
-            }
-        } catch (error) {
-            console.error('Error:', error);
         }
     };
 
@@ -136,49 +92,7 @@ const ProfileEditor = ({ navigate }) => {
                         <CollectionEditor />
                     }
                     {tab === 'ln' &&
-                        <form>
-                            <Grid container direction="column" alignItems="center">
-                                <Grid item>
-                                    <TextField
-                                        label="Lightning Address"
-                                        value={address}
-                                        onChange={(e) => setAddress(e.target.value)}
-                                        fullWidth
-                                        margin="normal"
-                                        sx={{ width: '20ch' }}
-                                    />
-                                </Grid>
-                                <Grid item>
-                                    <Box sx={{ marginTop: 2 }}>
-                                        <Grid container spacing={2}>
-                                            <Grid item>
-                                                <Button variant="contained" color="primary" onClick={handleSaveAddress}>
-                                                    Save
-                                                </Button>
-                                            </Grid>
-                                            {address &&
-                                                <Grid item>
-                                                    <Button
-                                                        variant="contained"
-                                                        color="primary"
-                                                        onClick={handleTestAddress}
-                                                        disabled={!!invoice}>
-                                                        Test
-                                                    </Button>
-                                                </Grid>
-                                            }
-                                        </Grid>
-                                    </Box>
-                                </Grid>
-                                {invoice &&
-                                    <Grid item>
-                                        <div>
-                                            <a href={invoice} onClick={() => setInvoice(null)}>⚡ zap 10 sats ⚡</a>
-                                        </div>
-                                    </Grid>
-                                }
-                            </Grid>
-                        </form>
+                        <LnAddressEditor profile={profile} />
                     }
                 </Box>
             </div>
