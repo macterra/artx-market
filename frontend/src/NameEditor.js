1fe326093d67ecf483f7f9102de48d63de828346
David McFadzean <davidmc@gmail.com>
2023-07-13 17:33:06 -0400

    Added NameEditor

---
+++ frontend/src/NameEditor.js
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+
+const NameEditor = ({ profile }) => {
+    const [name, setName] = useState(profile.name);
+    const [tagline, setTagline] = useState(profile.tagline);
+
+    const handleSaveClick = async () => {
+        try {
+            const response = await fetch('/api/profile', {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json', },
+                body: JSON.stringify({ name: name, tagline: tagline }),
+            });
+
+            if (response.ok) {
+                profile.name = name;
+                profile.tagline = tagline;
+            } else {
+                const data = await response.json();
+                console.error('Error updating profile:', data.message);
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error('Error updating profile:', error);
+        }
+    };
+
+    return (
+        <form>
+            <TextField
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Tagline"
+                value={tagline}
+                onChange={(e) => setTagline(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <Button variant="contained" color="primary" onClick={handleSaveClick}>
+                Save
+            </Button>
+        </form>
+    );
+};
+
+export default NameEditor;
--- frontend/src/ProfileEditor.js
+++ frontend/src/ProfileEditor.js
@@ -1,13 +1,13 @@
 
 import React, { useState, useEffect } from 'react';
-import { Box, Button, TextField, Tab, Tabs } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
+
+import NameEditor from './NameEditor';
 import CollectionEditor from './CollectionEditor';
 import LnAddressEditor from './LnAddressEditor';
 
 const ProfileEditor = ({ navigate }) => {
     const [profile, setProfile] = useState({});
-    const [name, setName] = useState('');
-    const [tagline, setTagline] = useState('');
     const [tab, setTab] = useState(null);
 
     useEffect(() => {
@@ -16,8 +16,6 @@ const ProfileEditor = ({ navigate }) => {
                 const response = await fetch(`/api/profile`);
                 const data = await response.json();
                 setProfile(data);
-                setName(data.name);
-                setTagline(data.tagline);
                 setTab("name");
             } catch (error) {
                 console.error('Error fetching profile data:', error);
@@ -27,27 +25,6 @@ const ProfileEditor = ({ navigate }) => {
 
         fetchProfile();
     }, []);
-
-    const handleSaveClick = async () => {
-        try {
-            const response = await fetch('/api/profile', {
-                method: 'PATCH',
-                headers: { 'Content-Type': 'application/json', },
-                body: JSON.stringify({ name: name, tagline: tagline }),
-            });
-
-            if (response.ok) {
-                profile.name = name;
-                profile.tagline = tagline;
-            } else {
-                const data = await response.json();
-                console.error('Error updating profile:', data.message);
-                alert(data.message);
-            }
-        } catch (error) {
-            console.error('Error updating profile:', error);
-        }
-    };
 
     return (
         <Box>
@@ -68,25 +45,7 @@ const ProfileEditor = ({ navigate }) => {
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Box style={{ width: '50vw' }}>
                     {tab === 'name' &&
-                        <form>
-                            <TextField
-                                label="Name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                fullWidth
-                                margin="normal"
-                            />
-                            <TextField
-                                label="Tagline"
-                                value={tagline}
-                                onChange={(e) => setTagline(e.target.value)}
-                                fullWidth
-                                margin="normal"
-                            />
-                            <Button variant="contained" color="primary" onClick={handleSaveClick}>
-                                Save
-                            </Button>
-                        </form>
+                        <NameEditor profile={profile} />
                     }
                     {tab === 'coll' &&
                         <CollectionEditor />
