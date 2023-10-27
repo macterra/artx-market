c76e5b29f3c29ecbe526a8dc3b82f3154aeff941
David McFadzean <davidmc@gmail.com>
2023-10-27 13:25:41 -0400

    Added support buttons to Footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Button } from "@mui/material";
 
+function openTab(url) {
+  window.open(url, "_blank");
+}
+
 function Footer() {
-  const handleDiscordButtonClick = () => {
-    const discordInviteLink = "https://discord.gg/RgZsQZqfp";
-
-    window.open(discordInviteLink, "_blank");
-  };
-
   return (
     <footer className="footer">
-      <Button color="inherit" onClick={handleDiscordButtonClick}>
-        Join Our Discord
+      <Button color="inherit" onClick={() => openTab("https://discord.gg/RgZsQZqfp")}>
+        Join our discord
+      </Button>
+      <Button color="inherit" onClick={() => openTab("https://github.com/macterra/artx-market/issues")}>
+        Report an issue
+      </Button>
+      <Button color="inherit" onClick={() => openTab("https://github.com/macterra/artx-market/wiki/Getting-Started")}>
+        Getting started
       </Button>
     </footer>
   );
