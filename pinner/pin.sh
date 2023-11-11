a62bc5d6fc08a0ea118e549ccd27a9285d19766c
David McFadzean <davidmc@gmail.com>
2023-11-11 11:56:44 -0500

    Fixed pinner startup

--- pinner/pin.sh
+++ pinner/pin.sh
@@ -3,12 +3,14 @@
 while true
 do
     docker exec artx-market-ipfs-1 ipfs add -r -Q /export/data > /tmp/CID
-    cp /tmp/CID /data
-
-    current_date=$(date)
     CID=$(cat /tmp/CID)
-    echo "$current_date: $CID"
-    echo "$current_date: $CID" >> /var/log/CID.log
+
+    if [[ -n "$CID" ]]; then
+        cp /tmp/CID /data
+        current_date=$(date)
+        echo "$current_date: $CID"
+        echo "$current_date: $CID" >> /var/log/CID.log
+    fi
 
     sleep 60
 done
