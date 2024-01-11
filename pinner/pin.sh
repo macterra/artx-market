f4e4fdacbb339d490c2e43e116d15a9311d5776d
David McFadzean <davidmc@gmail.com>
2024-01-10 20:39:57 -0500

    Added gc to pinner

--- pinner/pin.sh
+++ pinner/pin.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+counter=0
+
 while true
 do
     docker exec artx-market-ipfs-1 ipfs add -r -Q /export/data > /tmp/CID
     CID=$(cat /tmp/CID)
+    current_date=$(date)
 
     if [[ -n "$CID" ]]; then
         cp /tmp/CID /data
-        current_date=$(date)
         echo "$current_date: $CID"
         echo "$current_date: $CID" >> /var/log/CID.log
     fi
+
+    if (( counter % 60 == 0 )); then
+        echo "$current_date: running repo gc..."
+        docker exec artx-market-ipfs-1 ipfs repo gc
+    fi
+
+    ((counter++))
 
     sleep 60
 done
